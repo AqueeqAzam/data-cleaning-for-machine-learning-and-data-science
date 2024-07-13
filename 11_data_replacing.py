82f61400b9e63d41caf19f8d672c0fa3f107af27
AQUEEQ AZAM <84491720+AqueeqAzam@users.noreply.github.com>
2024-07-13 21:16:22 +0530

    Delete 11_data_replacing.py

--- 11_data_replacing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_csv("https://raw.githubusercontent.com/AqueeqAzam/DS-and-ML-datasets/main/test.csv")
-df.head(4)
-df.isnull().sum()
-df['experience'].value_counts()
-df['experience'].fillna(df['experience'].mode()[0], inplace=True)
-df.info()
